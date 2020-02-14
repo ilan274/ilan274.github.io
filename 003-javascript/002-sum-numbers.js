83b4b7dedf7c1e7c0924d4f4b4cd1995e4078709
ilan274 <ilan.herbach@gmail.com>
2020-02-14 16:33:05 -0300

    Initial JS commit with basic exercises

---
+++ 003-javascript/002-sum-numbers.js
@@ -0,0 +1,4 @@
+// Display the sum of 5 + 10, using two variables: x and y.
+let x = 5;
+let y = 10;
+document.getElementById("demo").innerHTML = x + y;
---
+++ 003-javascript/003-sum-and-display.js
@@ -0,0 +1,5 @@
+// Create a variable called z, assign x + y to it, and display the result.
+let x = 5;
+let y = 15;
+z = x + y;
+console.log(z)
