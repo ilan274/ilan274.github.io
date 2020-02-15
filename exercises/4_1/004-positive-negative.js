eaebe5a582e71703979cc5b7bb18025287987506
ilan274 <ilan.herbach@gmail.com>
2020-02-15 12:36:37 -0300

    Initial commit

---
+++ exercises/4_1/004-positive-negative.js
@@ -0,0 +1,13 @@
+// Make a program that, given a value defined in a variable, returns “positive” if that value is positive, “negative” if it is negative and “zero” otherwise.
+
+let num1 = 422;
+let num2 = -1;
+let num3 = 0
+
+if (num1 < 0) {
+  console.log("negative")
+} else if (num1 > 0) {
+  console.log("positve ")
+} else {
+  console.log("zero")
+}
