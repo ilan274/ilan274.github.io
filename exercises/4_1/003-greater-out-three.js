92caf6815dedd6bae84c6acba17e81ba6d8ca42a
ilan274 <ilan.herbach@gmail.com>
2020-02-15 11:13:08 -0300

    Initial commit

---
+++ exercises/4_1/003-greater-out-three.js
@@ -0,0 +1,11 @@
+let a = 25;
+let b = 500;
+let c = 231;
+
+if (a > b && a > c) {
+  console.log("Greater: " + a + ".")
+} else if (b > a && b > c) {
+  console.log("Greater: " + b + ".")
+} else {
+  console.log("Greater: " + c + ".")
+}
