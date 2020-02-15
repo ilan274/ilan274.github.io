7126e3bfcc023a0fd968b3adfc784cf199bc9e64
ilan274 <ilan.herbach@gmail.com>
2020-02-15 12:52:18 -0300

    Initial commit

---
+++ exercises/4_1/005-triangle-angles.js
@@ -0,0 +1,8 @@
+// Make a program that defines three variables with the values of the three internal angles of a triangle. Return true if the angles represent the angles of a triangle and false otherwise.
+
+let angle1 = 75;
+let angle2 = 63;
+let angle3 = 30;
+let result = angle1 + angle2 + angle3 == 180;
+
+console.log(result)
