a8095c42bff140638339db2c09969d9ffb9065a4
ilan274 <ilan.herbach@gmail.com>
2020-02-20 09:06:53 -0300

    Objects and for/in initial commit

---
+++ 4_3/002-add-key-value-object.js
@@ -0,0 +1,11 @@
+/* Insert a new property in the object with the key name “recurring” and the value “Yes” and then print the object on the console. */
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+info.recurring = ("Sim");
+
+console.log(info)
