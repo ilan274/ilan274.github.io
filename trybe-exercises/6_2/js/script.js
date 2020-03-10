2f6a3a74e027f4fe97eefea00843c403b54470c1
ilan274 <ilan.herbach@gmail.com>
2020-03-10 01:33:22 -0300

    Structure updates

--- trybe-exercises/6_2/js/script.js
+++ trybe-exercises/6_2/js/script.js
@@ -39,22 +39,3 @@ function addAllStates() {
   }
 }
 addAllStates();
-
-function validEmail(email) {
-  document.getElementById('email').style.backgroundColor = 'white';
-  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (re.test(document.getElementById('email').value) == false) {
-    document.getElementById('email').style.backgroundColor = 'red';
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function validacoes() {
-  if (validEmail()) {
-    return true;
-  }
-}
-
-getSubmitButton.addEventListener('click', validacoes);
