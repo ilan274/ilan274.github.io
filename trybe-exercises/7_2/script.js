e3c478a333a53d9b9705b90de3ffa2286658b774
ilan274 <ilan.herbach@gmail.com>
2020-03-14 19:42:12 -0300

    12th exercise

---
+++ trybe-exercises/7_2/script.js
@@ -0,0 +1,7 @@
+const getExercise12 = document.getElementById('exercise-12');
+
+function addClass() {
+  getExercise12.classList.add('disappear');
+}
+
+getExercise12.addEventListener('dblclick', addClass);
