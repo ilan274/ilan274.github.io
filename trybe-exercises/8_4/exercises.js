7905780d05b14c32f24087a55b83e9c223fdec71
ilan274 <ilan.herbach@gmail.com>
2020-03-24 12:20:52 -0300

    Initial commit

---
+++ trybe-exercises/8_4/exercises.js
@@ -0,0 +1,16 @@
+// First exercise
+const assert = require( 'assert' );
+// write solution below
+
+const addOne = arr => arr.map( num => num + 1 );
+
+// write solution above
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne( myArray );
+
+assert.strictEqual( typeof addOne, 'function' );
+assert.deepEqual( output, expected );
+assert.deepEqual( myArray, unchanged );
