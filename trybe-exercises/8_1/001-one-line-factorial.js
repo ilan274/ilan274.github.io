e238b56eb5823b5a50ca6495bae11952a1d54095
ilan274 <ilan.herbach@gmail.com>
2020-03-28 15:49:37 -0300

    Refactoring functions

--- trybe-exercises/8_1/001-one-line-factorial.js
+++ trybe-exercises/8_1/001-one-line-factorial.js
@@ -1,16 +1,20 @@
-const assert = require( 'assert' );
+const assert = require('assert');
 
-const factorial = n => {
-  if ( n < 0 ) throw new Error( 'Factorials must be positive numbers' );
-  return ( n === 0 || n === 1 ? 1 : n * factorial( n - 1 ) );
-};
+// Write code below
 
-assert.strictEqual( factorial( 0 ), 1 );
-assert.strictEqual( factorial( 1 ), 1 );
-assert.strictEqual( factorial( 2 ), 2 );
-assert.strictEqual( factorial( 3 ), 6 );
-assert.strictEqual( factorial( 4 ), 24 );
-assert.throws( () => {
-  factorial( -1 );
-}, /^Error: Factorials must be positive numbers$/ );
+const returnError = () => { throw new Error('Factorials must be positive numbers'); };
+const factorial = n => (n < 0) ? returnError() : (n === 0 || n === 1 ? 1 : n * factorial(n - 1));
+
+// Write code above
+
+console.log(factorial(5));
+
+assert.strictEqual(factorial(0), 1);
+assert.strictEqual(factorial(1), 1);
+assert.strictEqual(factorial(2), 2);
+assert.strictEqual(factorial(3), 6);
+assert.strictEqual(factorial(4), 24);
+assert.throws(() => {
+  factorial(-1);
+}, /^Error: Factorials must be positive numbers$/);
 
