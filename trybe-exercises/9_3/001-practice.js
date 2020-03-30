32d7311eb84cb168224ca7edb7e3f55dfe2c72fa
ilan274 <ilan.herbach@gmail.com>
2020-03-30 16:17:20 -0300

    001 practice solution

--- trybe-exercises/9_3/001-practice.js
+++ trybe-exercises/9_3/001-practice.js
@@ -1,15 +1,15 @@
-const nationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`
+const nationality = ({ firstName, nationality = 'Brazilian' }) => `${firstName} is ${nationality}`;
 
 const person = {
     firstName: "João",
     lastName: "Jr II"
-}
+};
 
 const otherPerson = {
     firstName: "Ivan",
     lastName: "Ivanovich",
     nationality: "Russian"
-}
+};
 
-console.log(nationality(otherPerson)) // Ivan is Russian
-console.log(nationality(person))
+console.log(nationality(otherPerson)); // Ivan is Russian
+console.log(nationality(person));
