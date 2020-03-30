1f804fc77bf0fab07ffeb5d9ea250696014c9b63
ilan274 <ilan.herbach@gmail.com>
2020-03-30 16:16:02 -0300

    Initial practice commit

---
+++ trybe-exercises/9_3/001-practice.js
@@ -0,0 +1,15 @@
+const nationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`
+
+const person = {
+    firstName: "João",
+    lastName: "Jr II"
+}
+
+const otherPerson = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+}
+
+console.log(nationality(otherPerson)) // Ivan is Russian
+console.log(nationality(person))
