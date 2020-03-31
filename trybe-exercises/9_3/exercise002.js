d53b01fee63c5817afe17e980a6861eacf948c13
ilan274 <ilan.herbach@gmail.com>
2020-03-30 23:03:29 -0300

    Second exercise

---
+++ trybe-exercises/9_3/exercise002.js
@@ -0,0 +1,19 @@
+const assert = require('assert')
+
+// Write function below
+
+
+const sum = (...n) => {
+  return n.reduce((acc, val) => {
+    return acc + val; 
+  }, 0)
+}
+
+// Write function above
+
+assert.equal(sum(), 0)
+assert.equal(sum(1), 1)
+assert.equal(sum(1, 2), 3)
+assert.equal(sum(1, 2, 3), 6)
+assert.equal(sum(1, 2, 3, 4), 10)
+    
